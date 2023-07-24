05be1fce2dfe865b6b252d90de4a33cb82de53fd
tinhnt <tinhnt@skylab.vn>
2023-07-24 23:51:31 +0700

    post answer of candidate

--- quiz-app/client/src/components/examTest/ExamTest.tsx
+++ quiz-app/client/src/components/examTest/ExamTest.tsx
@@ -20,27 +20,51 @@ import Typography from "@mui/joy/Typography";
 import useExam from "@/hooks/exam/useExam";
 import { useParams } from "react-router-dom";
 import CongratCard from "../CongratCrad";
+import { useForm } from "react-hook-form";
 function ExamTest() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const [orientation, setOrientation] =
     React.useState<ListProps["orientation"]>("vertical");
   const [examData, setExamData] = useState({});
   const [showCongrat, setShowCongrat] = useState(false);
   const { id: examId } = useParams();
   const { data } = useExam(Number(examId));
+
   const handleClose = () => {
     setShowCongrat(false);
+  };
+  const [value, setValue] = React.useState({});
+
+  const handleRadioChange = (questionIndex: any) => (event: any) => {
+    setValue({
+      ...value,
+      [questionIndex]: event.target.value,
+    });
+  };
+
+  // ...
+
+  const onSubmit = (data) => {
+    console.log(data);
+    setShowCongrat(true);
+    console.log("submit");
   };
   useEffect(() => {
     setExamData({ ...data });
   }, [data]);
   console.log(data?.questions?.options);
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // check exam condition
-    setShowCongrat(true); // show CongratCard when exam conditions meet
-  };
+  // const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  //   event.preventDefault();
+  //   console.log(value);
+  //   // check exam condition
+  //   setShowCongrat(true); // show CongratCard when exam conditions meet
+  // };
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <Card style={{ padding: "50px" }}>
         <CardContent>
           <Typography variant="h4">Take an exam {examData.name}</Typography>
@@ -61,6 +85,7 @@ function ExamTest() {
                 >
                   Question {index + 1}: {item?.name}
                 </Typography>
+                {errors[`${item.id}`] && <span>This field is required</span>}
               </Grid>
               <Grid item md={12} xs={12}>
                 <RadioGroup
@@ -83,7 +108,15 @@ function ExamTest() {
                       <React.Fragment key={value}>
                         {index !== 0 && <ListDivider />}
                         <ListItem>
-                          <Radio id={value} value={value} label={value} />
+                          <Radio
+                            id={value}
+                            value={value}
+                            label={value}
+                            onChange={handleRadioChange(item.id)}
+                            {...register(`${item.id}`, {
+                              required: true,
+                            })}
+                          />
                         </ListItem>
                       </React.Fragment>
                     ))}
@@ -104,12 +137,7 @@ function ExamTest() {
           mt: 3,
         }}
       >
-        <Button
-          sx={{ m: 1 }}
-          type="submit"
-          variant="contained"
-          onClick={handleSubmit}
-        >
+        <Button sx={{ m: 1 }} type="submit" variant="contained">
           Save
         </Button>
       </Box>
